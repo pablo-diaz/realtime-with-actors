2a8775642c0d8d2288bce0464801f90a9a9dd7ff
Pablo Diaz <pablo.diaz@yuxiglobal.com>
2025-02-19 14:09:45 -0500

    Fixing a bug

--- src/DeviceStateApi/Infrastructure/EventHandlers/PersistenceForAllDeviceEvents.cs
+++ src/DeviceStateApi/Infrastructure/EventHandlers/PersistenceForAllDeviceEvents.cs
@@ -2,14 +2,19 @@
 using System.Threading;
 using System.Threading.Tasks;
 
-using DeviceStateServices;
 using Domain.Events;
+using DeviceStateServices;
 
 using MediatR;
 
 namespace DeviceStateApi.Infrastructure.EventHandlers;
 
-public sealed class PersistenceForAllDeviceEvents : INotificationHandler<DeviceEvent>
+public sealed class PersistenceForAllDeviceEvents : INotificationHandler<DeviceHasBeenCreated>
+                                                  , INotificationHandler<DeviceLocationHasChanged>
+                                                  , INotificationHandler<DeviceLocationHasChangedToAVeryCloseLocation>
+                                                  , INotificationHandler<DeviceTemperatureHasDecreased>
+                                                  , INotificationHandler<DeviceTemperatureHasIncreased>
+                                                  , INotificationHandler<SimilarDeviceTemperatureWasTraced>  // implementing all these interfaces was a Hack, because it wouldn't work with INotificationHandler<DeviceEvent>
 {
     private readonly IEventStore _eventStore;
 
@@ -18,6 +23,21 @@ public sealed class PersistenceForAllDeviceEvents : INotificationHandler<DeviceE
         this._eventStore = eventStore;
     }
 
-    public Task Handle(DeviceEvent @event, CancellationToken cancellationToken) =>
+    public Task Handle(DeviceEvent @event, CancellationToken cancellationToken) => Store(@event);
+
+    public Task Handle(DeviceHasBeenCreated @event, CancellationToken cancellationToken) => Store(@event);
+
+    public Task Handle(DeviceLocationHasChanged @event, CancellationToken cancellationToken) => Store(@event);
+
+    public Task Handle(DeviceLocationHasChangedToAVeryCloseLocation @event, CancellationToken cancellationToken) => Store(@event);
+
+    public Task Handle(DeviceTemperatureHasDecreased @event, CancellationToken cancellationToken) => Store(@event);
+
+    public Task Handle(DeviceTemperatureHasIncreased @event, CancellationToken cancellationToken) => Store(@event);
+
+    public Task Handle(SimilarDeviceTemperatureWasTraced @event, CancellationToken cancellationToken) => Store(@event);
+
+    private Task Store(DeviceEvent @event) =>
         _eventStore.StoreEvent(@event, at: DateTimeOffset.UtcNow);
+
 }
